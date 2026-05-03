ccbb060085a44b2ca5ae21a35170aa86187aaff5
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-05-03 22:48:12 +0100

    updated js

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
 const cards = document.querySelectorAll(".card");
 const lists = document.querySelectorAll(".list");
-const cards = document.querySelectorAll(".card");{}
 
-for 
+for(const card of cards){
+    card.addEventListener("dragstart", dragStart)
+    card.addEventListener("dragend", dragEnd)
+}
+
+for (const list of lists) {
+    list.addEventListener("dragover", dragOver)
+    list.addEventListener("dragenter", dragEnter)
+    list.addEventListener("dragleave", dragLeave)
+    list.addEventListener("drop", dragDrop)
+
+}
+
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain", this.id)
+    
+}
+
+function dragEnd() {
+    console.log("Drag Ended");
+    
+}
+
+function dragLeave(e) {
+    
+    e.preventDefault();
+
+    this.classList.add("over");
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.classList.add("over")
+    
+}
+
+function dragOver(e) {
+
+    this.classList.remove("over");
+}
+
+
+function dragDrop(e) {
+    const id = e.dataTransfer.getData("text/plain")
+
+    const card = document.getElementById(id);
+
+    
+}
